fee6d951dabb6ec3933b95ec41c84b784ba395ee
Arshamdnt <arsham6ta@gmail.com>
2023-03-22 19:42:03 +0430

    empty

--- script.js
+++
@@ -1,7 +0,0 @@
-
-var userNames = ['Ali', 'Amin', 'Amir', 'roz', 'Babak']
-var filteredUserNames = userNames.filter(function (username) {
-  return username.length > 3
-})
-
-console.log(filteredUserNames)
